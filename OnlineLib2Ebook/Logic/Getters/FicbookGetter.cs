f0d5bc956ca603ad5477c976c8f0ac3378452529
okoloskov <koloskov_oleg@mail.ru>
2021-12-26 21:41:35 +0300

    Fix no toc fanfics

--- OnlineLib2Ebook/Logic/Getters/FicbookGetter.cs
+++ OnlineLib2Ebook/Logic/Getters/FicbookGetter.cs
@@ -25,10 +25,11 @@ namespace OnlineLib2Ebook.Logic.Getters {
             var uri = new Uri($"https://ficbook.net/readfic/{bookId}");
             var doc = await _config.Client.GetHtmlDocWithTriesAsync(uri);
 
+            var title = doc.GetTextBySelector("h1.mb-10");
             var book = new Book {
                 Cover = await GetCover(doc, uri),
-                Chapters = await FillChapters(doc, url),
-                Title = doc.GetTextBySelector("h1.mb-10"),
+                Chapters = await FillChapters(doc, url, title),
+                Title = title,
                 Author = doc.GetTextBySelector("a.creator-nickname")
             };
             
@@ -40,10 +41,10 @@ namespace OnlineLib2Ebook.Logic.Getters {
             return !string.IsNullOrWhiteSpace(imagePath) ? GetImage(new Uri(bookUri, imagePath)) : Task.FromResult(default(Image));
         }
 
-        private async Task<IEnumerable<Chapter>> FillChapters(HtmlDocument doc, Uri url) {
+        private async Task<IEnumerable<Chapter>> FillChapters(HtmlDocument doc, Uri url, string title) {
             var result = new List<Chapter>();
             
-            foreach (var ficbookChapter in GetChapters(doc, url)) {
+            foreach (var ficbookChapter in GetChapters(doc, url, title)) {
                 Console.WriteLine($"Загружаем главу \"{ficbookChapter.Title}\"");
                 var chapter = new Chapter();
                 var chapterDoc = await GetChapter(ficbookChapter);
@@ -79,11 +80,16 @@ namespace OnlineLib2Ebook.Logic.Getters {
             return text.ToString().HtmlDecode().AsHtmlDoc();
         }
 
-        private static IEnumerable<UrlChapter> GetChapters(HtmlDocument doc, Uri url) {
-            foreach (var li in doc.QuerySelectorAll("li.part")) {
-                var a = li.QuerySelector("a.part-link.visit-link");
-                if (a != null) {
-                    yield return new UrlChapter(new Uri(url, a.Attributes["href"].Value), li.GetTextBySelector("h3"));
+        private static IEnumerable<UrlChapter> GetChapters(HtmlDocument doc, Uri url, string title) {
+            var links = doc.QuerySelectorAll("li.part");
+            if (links.Count == 0) {
+                yield return new UrlChapter(url, title);
+            } else {
+                foreach (var li in links) {
+                    var a = li.QuerySelector("a.part-link.visit-link");
+                    if (a != null) {
+                        yield return new UrlChapter(new Uri(url, a.Attributes["href"].Value), li.GetTextBySelector("h3"));
+                    }
                 }
             }
         }
