b366e756980b53af37a593f432156e02ddb8199f
Oleg Koloskov <oleg.koloskov@ingate.ru>
2022-09-05 01:40:45 +0300

    https://acomics.ru AgeRestrict

--- Elib2Ebook/Logic/Getters/AcomicsGetter.cs
+++ Elib2Ebook/Logic/Getters/AcomicsGetter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Elib2Ebook.Configs;
@@ -22,14 +23,16 @@ public class AcomicsGetter : GetterBase{
     public override async Task<Book> Get(Uri url) {
         var id = GetId(url);
         url = new Uri($"https://acomics.ru/{id}");
-        var doc = await Config.Client.GetHtmlDocWithTriesAsync(url);
+        await AgeRestict(id);
+        
+        var doc = await Config.Client.GetHtmlDocWithTriesAsync(new Uri(SystemUrl, $"{id}/1"));
 
         var title = doc.QuerySelector("meta[property=og:title]").Attributes["content"].Value;
         var book = new Book(url) {
-            Chapters = await FillChapters(id, title, url),
+            Chapters = await FillChapters(doc, id, title, url),
             Title = title,
             Author = GetAuthor(doc, url),
-            Annotation = GetAnnotation(doc)
+            Annotation = doc.QuerySelector("div.serial-description div.s-gap")?.NextSibling?.InnerHtml
         };
 
         book.Cover = book.Chapters.FirstOrDefault()?.Images.FirstOrDefault();
@@ -37,31 +40,25 @@ public class AcomicsGetter : GetterBase{
         return book;
     }
 
-    private static string GetAnnotation(HtmlDocument doc) {
-        return doc.QuerySelector("div.serial-description div.s-gap")?.NextSibling?.InnerHtml ?? doc.QuerySelector("div.about-summary p")?.InnerHtml;
+    private Task AgeRestict(string id) {
+        var data = new Dictionary<string, string> {
+            ["ageRestrict"] = "17",
+        };
+
+
+        return Config.Client.PostWithTriesAsync(new Uri(SystemUrl, id), new FormUrlEncodedContent(data));
     }
 
     private static Author GetAuthor(HtmlDocument doc, Uri url) {
         var a = doc.QuerySelector("article.authors a.username");
-        if (a == default) {
-            foreach (var p in doc.QuerySelectorAll("div.about-summary p")) {
-                var b = p.GetTextBySelector("b");
-                if (b == "Автор:") {
-                    a = p.QuerySelector("a");
-                    return new Author(a.GetText(), new Uri(url, a.Attributes["href"].Value));
-                }
-            }
-        }
-        
         return new Author(a.GetText(), new Uri(url, a.Attributes["href"].Value));
     }
 
-    private async Task<IEnumerable<Chapter>> FillChapters(string bookId, string title, Uri url) {
+    private async Task<IEnumerable<Chapter>> FillChapters(HtmlDocument doc, string bookId, string title, Uri url) {
         var chapter = new Chapter {
             Title = title
         };
-
-        var doc = await Config.Client.GetHtmlDocWithTriesAsync(new Uri(SystemUrl, $"{bookId}/{1}"));
+        
         var pages = int.Parse(doc.GetTextBySelector("span.issueNumber").Split("/").Last());
         var sb = new StringBuilder();
         for (var i = 1; i <= pages; i++) {
