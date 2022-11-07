a0868b7b0bd65ec888280ea4c72d23999168da39
Oleg Koloskov <oleg.koloskov@ingate.ru>
2022-11-07 15:10:27 +0300

    https://noveltranslate.com/ fixes:
    * get full toc
    * fix lang settings

--- Elib2Ebook/Logic/Getters/NovelTranslateGetter.cs
+++ Elib2Ebook/Logic/Getters/NovelTranslateGetter.cs
@@ -38,7 +38,7 @@ public class NovelTranslateGetter : GetterBase {
 
         var book = new Book(url) {
             Cover = await GetCover(doc, url),
-            Chapters = await FillChapters(doc, url),
+            Chapters = await FillChapters(doc, lang, url),
             Title = doc.GetTextBySelector("h1"),
             Author = GetAuthor(doc, url),
             Annotation = GetAnnotation(doc),
@@ -48,10 +48,10 @@ public class NovelTranslateGetter : GetterBase {
         return book;
     }
 
-    private async Task<IEnumerable<Chapter>> FillChapters(HtmlDocument doc, Uri url) {
+    private async Task<IEnumerable<Chapter>> FillChapters(HtmlDocument doc, string lang, Uri url) {
         var result = new List<Chapter>();
 
-        foreach (var urlChapter in GetToc(doc, url)) {
+        foreach (var urlChapter in await GetToc(doc, lang, url)) {
             Console.WriteLine($"Загружаю главу {urlChapter.Title.CoverQuotes()}");
 
             var chapter = new Chapter {
@@ -70,13 +70,26 @@ public class NovelTranslateGetter : GetterBase {
 
     private async Task<HtmlDocument> GetChapter(UrlChapter urlChapter) {
         var doc = await Config.Client.GetHtmlDocWithTriesAsync(urlChapter.Url);
-        return doc.QuerySelector("div.reading-content div.text-left").InnerHtml.AsHtmlDoc();
+        return doc.QuerySelector("div.reading-content div.text-left").RemoveNodes("div.novel-before-content, div.tptn_counter, div.novel-after-content").InnerHtml.AsHtmlDoc();
     }
 
-    private IEnumerable<UrlChapter> GetToc(HtmlDocument doc, Uri url) {
-        var result = doc.QuerySelectorAll("div.listing-chapters_wrap li.wp-manga-chapter a").Select(a => new UrlChapter(url.MakeRelativeUri(a.Attributes["href"].Value), a.GetText())).ToList();
-        result.Reverse();
-        return SliceToc(result);
+    private async Task<IEnumerable<UrlChapter>> GetToc(HtmlDocument doc, string lang, Uri url) {
+        var lastChapter = url.MakeRelativeUri(doc.QuerySelector("div.listing-chapters_wrap li.wp-manga-chapter a").Attributes["href"].Value);
+        var chapterDoc = await Config.Client.GetHtmlDocWithTriesAsync(lastChapter);
+
+        var chapters = new List<UrlChapter>();
+        foreach (var option in chapterDoc.QuerySelectorAll("select.selectpicker_chapter option")) {
+            var name = option.GetText();
+            var chapterUri = option.Attributes["data-redirect"].Value.AsUri();
+            if (!chapterUri.LocalPath.Trim('/').StartsWith(lang)) {
+                chapterUri = url.MakeRelativeUri($"/{lang}/{chapterUri.LocalPath.Trim('/')}");
+            }
+            
+            chapters.Add(new UrlChapter(chapterUri, name));
+        }
+
+        chapters.Reverse();
+        return SliceToc(chapters);
     }
 
     private static string GetAnnotation(HtmlDocument doc) {
@@ -84,7 +97,7 @@ public class NovelTranslateGetter : GetterBase {
     }
 
     private Task<Image> GetCover(HtmlDocument doc, Uri uri) {
-        var imagePath = doc.QuerySelector("div.summary_image img")?.Attributes["src"]?.Value;
+        var imagePath = doc.QuerySelector("div.summary_image img")?.Attributes["data-lazy-src"]?.Value;
         return !string.IsNullOrWhiteSpace(imagePath) ? SaveImage(uri.MakeRelativeUri(imagePath)) : Task.FromResult(default(Image));
     }
     
