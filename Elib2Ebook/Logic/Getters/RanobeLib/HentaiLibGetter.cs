7730749ce7ee94b36d6b52d909d2663689ea627d
Oleg Koloskov <oleg.koloskov@ingate.ru>
2022-11-19 22:48:06 +0300

    get main server for https://mangalib.me/

--- Elib2Ebook/Logic/Getters/RanobeLib/HentaiLibGetter.cs
+++ Elib2Ebook/Logic/Getters/RanobeLib/HentaiLibGetter.cs
@@ -8,8 +8,4 @@ namespace Elib2Ebook.Logic.Getters.RanobeLib;
 public class HentaiLibGetter : MangaLibGetterBase {
     public HentaiLibGetter(BookGetterConfig config) : base(config) { }
     protected override Uri SystemUrl => new("https://hentailib.me");
-
-    protected override string GetImageUrl(Uri url, RanobeLibChapter chapter, MangaLibPg p) {
-        return $"https://img3.hentailib.org/manga/{GetId(url)}/chapters/{chapter.ChapterSlug}/{p.U}";
-    }
 }
--- Elib2Ebook/Logic/Getters/RanobeLib/MangaLibGetterBase.cs
+++ Elib2Ebook/Logic/Getters/RanobeLib/MangaLibGetterBase.cs
@@ -22,25 +22,40 @@ public abstract class MangaLibGetterBase : RanobeLibGetterBase {
         return pg.OrderBy(p => p.P);
     }
 
+    private static Uri GetImgServer(HtmlDocument doc) {
+        var defaultServer = "https://img33.imgslib.link".AsUri();
+        
+        var match = Regex.Match(doc.ParsedText, "\"servers\":(?<servers>{.*?})");
+        if (match.Success) {
+            var servers = match.Groups["servers"].Value.Deserialize<Dictionary<string, string>>();
+            if (servers.TryGetValue("main", out var main)) {
+                return main.AsUri();
+            }
+        }
+        
+        Console.WriteLine($"Не удалось определить основной сервер изображений. Использую {defaultServer}");
+        return defaultServer;
+    }
+
     protected override async Task<HtmlDocument> GetChapter(Uri url, RanobeLibChapter chapter) {
         var chapterDoc = await Config.Client.GetHtmlDocWithTriesAsync(url.AppendSegment($"/v{chapter.ChapterVolume}/c{chapter.ChapterNumber}?bid={chapter.BranchId}"));
         var header = chapterDoc.QuerySelector("div.auth-form-title");
         if (header != default && header.GetText() == "Авторизация") {
             throw new Exception("Произведение доступно только зарегистрированным пользователям. Добавьте в параметры вызова свои логин и пароль");
         }
-        
+
         var pg = GetPg(chapterDoc);
         var sb = new StringBuilder();
 
         foreach (var p in pg) {
-            var imageUrl = GetImageUrl(url, chapter, p);
+            var imageUrl = GetImageUrl(GetImgServer(chapterDoc), url, chapter, p);
             sb.Append($"<img src='{imageUrl}'/>");
         }
 
         return sb.AsHtmlDoc();
     }
 
-    protected virtual string GetImageUrl(Uri url, RanobeLibChapter chapter, MangaLibPg p) {
-        return $"https://img33.imgslib.link/manga/{GetId(url)}/chapters/{chapter.ChapterSlug}/{p.U}";
+    private string GetImageUrl(Uri imageServer, Uri url, RanobeLibChapter chapter, MangaLibPg p) {
+        return imageServer.MakeRelativeUri($"/manga/{GetId(url)}/chapters/{chapter.ChapterSlug}/{p.U}").ToString();
     }
 }
