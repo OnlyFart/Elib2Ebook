110c0c6824263ac886fb0e4541e1a5b3d934d742
RedBuld <sibiryakov.ya@gmail.com>
2023-01-27 10:26:28 +0300

    Update BookGetterConfig.cs

--- Elib2Ebook/Configs/BookGetterConfig.cs
+++ Elib2Ebook/Configs/BookGetterConfig.cs
@@ -20,6 +20,15 @@ public class BookGetterConfig : IDisposable {
         CookieContainer = cookieContainer;
         Options = options;
         TempFolder = tempFolder;
+
+        Client.DefaultRequestHeaders.Add("sec-ch-ua","\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"");
+        Client.DefaultRequestHeaders.Add("sec-ch-ua-mobile","?0");
+        Client.DefaultRequestHeaders.Add("sec-ch-ua-platform","linux");
+        Client.DefaultRequestHeaders.Add("sec-fetch-dest","document");
+        Client.DefaultRequestHeaders.Add("sec-fetch-mode","navigate");
+        Client.DefaultRequestHeaders.Add("sec-fetch-site","none");
+        Client.DefaultRequestHeaders.Add("sec-fetch-user","?1");
+        Client.DefaultRequestHeaders.Add("user-agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
     }
 
     public void Dispose() {
