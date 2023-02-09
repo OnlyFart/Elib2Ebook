8341d160e33e6a7954ecc95546dcecf1e52a8db0
Oleg Koloskov <oleg.koloskov@ingate.ru>
2023-02-09 23:11:30 +0300

    fix https://dark-novels.ru

--- Elib2Ebook/Logic/Getters/DarkNovelsGetter.cs
+++ Elib2Ebook/Logic/Getters/DarkNovelsGetter.cs
@@ -32,7 +32,7 @@ public class DarkNovelsGetter : GetterBase {
 
     public override Task Init() {
         Config.Client.DefaultRequestVersion = HttpVersion.Version20;
-        Config.Client.DefaultRequestHeaders.Add("User-Agent", "novels");
+        Config.Client.DefaultRequestHeaders.Add("User-Agent", "novels 1");
         return Task.CompletedTask;
     }
 
