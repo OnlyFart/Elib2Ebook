9bad415480759f9bc685889ee40406728fcca1e0
Oleg Koloskov <oleg.koloskov@ingate.ru>
2024-09-03 16:23:21 +0300

    fix

--- Elib2EbookWeb/Misc/SbWriter.cs
+++ Elib2EbookWeb/Misc/SbWriter.cs
@@ -3,12 +3,16 @@ using System.Text;
 namespace Elib2EbookWeb.Misc;
 
 public class SbLogger : ILogger {
-    private readonly StringBuilder _builder;
+    public readonly StringBuilder Builder;
     private readonly Action _action;
 
-    public SbLogger(StringBuilder builder, Action action) {
-        _builder = builder;
+    public SbLogger(Action action) : this(action, new StringBuilder()) {
+
+    }
+    
+    public SbLogger(Action action, StringBuilder builder) {
         _action = action;
+        Builder = builder;
     }
     
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull => default!;
@@ -16,7 +20,7 @@ public class SbLogger : ILogger {
     public bool IsEnabled(LogLevel logLevel) => true;
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) {
-        _builder.AppendLine($"{formatter(state, exception)}");
+        Builder.AppendLine($"{formatter(state, exception)}");
         _action();
     }
 }
--- Elib2EbookWeb/Program.cs
+++ Elib2EbookWeb/Program.cs
@@ -1,11 +1,22 @@
 using Elib2EbookWeb.Components;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config => {
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 var app = builder.Build();
 
